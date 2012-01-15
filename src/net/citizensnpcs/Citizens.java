e1d3cd4f313c5326953103be1eb41fb299a07cf7
aPunch <alexpuncochar@yahoo.com>
2012-01-14 23:09:31 -0600

    go back go back go back, go back to where we were

--- src/net/citizensnpcs/Citizens.java
+++ src/net/citizensnpcs/Citizens.java
@@ -1,13 +1,11 @@
 package net.citizensnpcs;
 
 import net.citizensnpcs.api.CitizensAPI;
-import net.citizensnpcs.api.CitizensPlugin;
-import net.citizensnpcs.api.npc.NPCManager;
 import net.citizensnpcs.npc.CitizensNPCManager;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Citizens extends JavaPlugin implements CitizensPlugin {
+public class Citizens extends JavaPlugin {
 
 	@Override
 	public void onDisable() {
@@ -15,11 +13,6 @@ public class Citizens extends JavaPlugin implements CitizensPlugin {
 
 	@Override
 	public void onEnable() {
-		CitizensAPI.setInstance(this);
-	}
-
-	@Override
-	public NPCManager getNPCManager() {
-		return new CitizensNPCManager();
+		CitizensAPI.setNPCManager(new CitizensNPCManager());
 	}
 }
