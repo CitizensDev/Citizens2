1533cd6eff79ecdb3cdb999d5ce519464cbd1313
aPunch <alexpuncochar@yahoo.com>
2012-01-15 21:15:32 -0600

    implemented saving framework, messaging utils

--- src/net/citizensnpcs/Citizens.java
+++ src/net/citizensnpcs/Citizens.java
@@ -2,6 +2,7 @@ package net.citizensnpcs;
 
 import net.citizensnpcs.api.CitizensAPI;
 import net.citizensnpcs.npc.CitizensNPCManager;
+import net.citizensnpcs.util.Messaging;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -9,10 +10,15 @@ public class Citizens extends JavaPlugin {
 
 	@Override
 	public void onDisable() {
+		Messaging.log("v" + getDescription().getVersion() + " disabled.");
 	}
 
 	@Override
 	public void onEnable() {
 		CitizensAPI.setNPCManager(new CitizensNPCManager());
+
+		// TODO wait to load until after all plugins using CitizensAPI are
+		// loaded
+		Messaging.log("v" + getDescription().getVersion() + " enabled.");
 	}
 }
