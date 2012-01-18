4503a6dce83e1f8b9f6789e46e4ec33280bcf32d
aPunch <alexpuncochar@yahoo.com>
2012-01-18 04:35:15 -0600

    added some events

---
+++ src/net/citizensnpcs/listener/EntityListen.java
@@ -0,0 +1,40 @@
+package net.citizensnpcs.listener;
+
+import net.citizensnpcs.api.CitizensAPI;
+import net.citizensnpcs.api.npc.NPC;
+
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityTargetEvent;
+
+public class EntityListen implements Listener {
+
+	@EventHandler(event = EntityDamageEvent.class, priority = EventPriority.NORMAL)
+	public void onEntityDamage(EntityDamageEvent event) {
+		if (event.isCancelled() || !CitizensAPI.getNPCManager().isNPC((LivingEntity) event.getEntity()))
+			return;
+
+		if (event instanceof EntityDamageByEntityEvent) {
+			EntityDamageByEntityEvent e = (EntityDamageByEntityEvent) event;
+			if (e.getDamager() instanceof Player) {
+				NPC npc = CitizensAPI.getNPCManager().getNPC((LivingEntity) event.getEntity());
+				npc.getCharacter().onLeftClick(npc, (Player) e.getDamager());
+			}
+		}
+	}
+
+	@EventHandler(event = EntityTargetEvent.class, priority = EventPriority.NORMAL)
+	public void onEntityTarget(EntityTargetEvent event) {
+		if (event.isCancelled() || !CitizensAPI.getNPCManager().isNPC((LivingEntity) event.getEntity())
+				|| !(event.getTarget() instanceof Player))
+			return;
+
+		NPC npc = CitizensAPI.getNPCManager().getNPC((LivingEntity) event.getEntity());
+		npc.getCharacter().onRightClick(npc, (Player) event.getTarget());
+	}
+}
