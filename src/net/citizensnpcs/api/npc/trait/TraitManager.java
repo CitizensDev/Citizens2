38d8ad20f4a7b403ad9cd129d0a6ee6e6316ad1a
fullwall <fullwall@optusnet.com>
2012-01-22 15:32:26 +0800

    Allow easier trait registration

--- src/net/citizensnpcs/api/npc/trait/TraitManager.java
+++ src/net/citizensnpcs/api/npc/trait/TraitManager.java
@@ -3,14 +3,36 @@ package net.citizensnpcs.api.npc.trait;
 import net.citizensnpcs.api.Factory;
 
 public interface TraitManager {
-
-    public void registerTraitFactory(String name, Factory<? extends Trait> factory);
+    /**
+     * Registers a trait with the given name. This will create a factory for
+     * instantiating the {@link Trait}s.
+     * 
+     * Note that this may be slower than
+     * {@link TraitManager#registerTraitWithFactory(String, Factory)}.
+     * 
+     * @param name
+     *            The name of the trait
+     * @param clazz
+     *            The trait's class
+     */
+    public void registerTrait(String name, Class<? extends Trait> clazz);
 
     /**
-     * Gets a trait from the given name
+     * Registers an {@link Trait} with the given name. New instances of the
+     * trait will be constructed with the given {@link Factory}.
      * 
-     * @param trait
-     *            Trait to register
+     * @param name
+     *            The name of the trait
+     * @param factory
+     *            The factory to instantiate new instances
+     */
+    public void registerTraitWithFactory(String name, Factory<? extends Trait> factory);
+
+    /**
+     * Constructs a trait from the given name
+     * 
+     * @param name
+     *            Name of the trait
      */
     public Trait getTrait(String name);
 }
