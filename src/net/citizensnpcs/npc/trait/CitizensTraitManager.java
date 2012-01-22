c221abc75dfabaacaeafd989522ecc564254accb
fullwall <fullwall@optusnet.com>
2012-01-22 15:31:49 +0800

    Allow easier trait registration

--- src/net/citizensnpcs/npc/trait/CitizensTraitManager.java
+++ src/net/citizensnpcs/npc/trait/CitizensTraitManager.java
@@ -20,9 +20,31 @@ public class CitizensTraitManager implements TraitManager {
     }
 
     @Override
-    public void registerTraitFactory(String name, Factory<? extends Trait> factory) {
+    public void registerTrait(String name, Class<? extends Trait> clazz) {
+        registerTraitWithFactory(name, new ReflectionFactory(clazz));
+    }
+
+    @Override
+    public void registerTraitWithFactory(String name, Factory<? extends Trait> factory) {
         if (registered.get(name) != null)
             throw new IllegalArgumentException("trait factory already registered");
         registered.put(name, factory);
+    }
+
+    private static class ReflectionFactory implements Factory<Trait> {
+        private final Class<? extends Trait> clazz;
+
+        private ReflectionFactory(Class<? extends Trait> clazz) {
+            this.clazz = clazz;
+        }
+
+        @Override
+        public Trait create() {
+            try {
+                return clazz.newInstance();
+            } catch (Exception ex) {
+                return null;
+            }
+        }
     }
 }
