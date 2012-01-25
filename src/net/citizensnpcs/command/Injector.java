4e7b2efc4f177212ce7be3166f006ecd925cafcb
aPunch <alexpuncochar@yahoo.com>
2012-01-25 14:07:49 -0600

    some more commands

--- src/net/citizensnpcs/command/Injector.java
+++ src/net/citizensnpcs/command/Injector.java
@@ -6,10 +6,46 @@
 
 package net.citizensnpcs.command;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.logging.Level;
 
-public interface Injector {
+import net.citizensnpcs.util.Messaging;
 
-    public Object getInstance(Class<?> cls) throws InvocationTargetException, IllegalAccessException,
-            InstantiationException;
+public class Injector {
+
+    private Object[] args;
+    private Class<?>[] argClasses;
+
+    public Injector(Object... args) {
+        this.args = args;
+        argClasses = new Class[args.length];
+        for (int i = 0; i < args.length; ++i) {
+            argClasses[i] = args[i].getClass();
+        }
+    }
+
+    public Object getInstance(Class<?> clazz) {
+        try {
+            Constructor<?> ctr = clazz.getConstructor(argClasses);
+            ctr.setAccessible(true);
+            return ctr.newInstance(args);
+        } catch (NoSuchMethodException e) {
+            Messaging.log(Level.SEVERE, "Error initializing commands class " + clazz + ": ");
+            e.printStackTrace();
+            return null;
+        } catch (InvocationTargetException e) {
+            Messaging.log(Level.SEVERE, "Error initializing commands class " + clazz + ": ");
+            e.printStackTrace();
+            return null;
+        } catch (InstantiationException e) {
+            Messaging.log(Level.SEVERE, "Error initializing commands class " + clazz + ": ");
+            e.printStackTrace();
+            return null;
+        } catch (IllegalAccessException e) {
+            Messaging.log(Level.SEVERE, "Error initializing commands class " + clazz + ": ");
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
