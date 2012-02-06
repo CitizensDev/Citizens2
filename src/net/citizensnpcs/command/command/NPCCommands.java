89bd263aad39821db6a52b6e173d76c9219c6799
aPunch <alexpuncochar@yahoo.com>
2012-02-06 05:58:22 -0600

    whoops

--- src/net/citizensnpcs/command/command/NPCCommands.java
+++ src/net/citizensnpcs/command/command/NPCCommands.java
@@ -55,7 +55,7 @@ public class NPCCommands {
                 Messaging.sendError(player, "'" + args.getString(2) + "' is not a valid mob type. Using default NPC.");
             }
         NPC create = npcManager.createNPC(type, name);
-        String successMsg = ChatColor.GREEN + "You created " + create.getName();
+        String successMsg = ChatColor.GREEN + "You created " + StringHelper.wrap(create.getName());
         boolean success = true;
         if (args.argsLength() == 4) {
             if (characterManager.getInstance(args.getString(3), create) == null) {
