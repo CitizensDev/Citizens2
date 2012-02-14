81ad6077aa5de00be77607c1533243757ca88684
fullwall <fullwall@optusnet.com>
2012-02-14 18:53:28 +0800

    Changes to inventories

--- src/net/citizensnpcs/api/npc/trait/trait/Inventory.java
+++ src/net/citizensnpcs/api/npc/trait/trait/Inventory.java
@@ -14,14 +14,13 @@ import org.bukkit.inventory.ItemStack;
 
 @SaveId("inventory")
 public class Inventory extends Trait {
-    private ItemStack[] contents;
+    private org.bukkit.inventory.Inventory inventory;
 
     public Inventory() {
-        contents = new ItemStack[36];
     }
 
     public Inventory(org.bukkit.inventory.Inventory inventory) {
-        contents = inventory.getContents();
+        this.inventory = inventory;
     }
 
     /**
@@ -30,7 +29,7 @@ public class Inventory extends Trait {
      * @return ItemStack array of an NPC's inventory contents
      */
     public ItemStack[] getContents() {
-        return contents;
+        return inventory.getContents();
     }
 
     /**
@@ -40,18 +39,18 @@ public class Inventory extends Trait {
      *            ItemStack array to set as the contents of an NPC's inventory
      */
     public void setContents(ItemStack[] contents) {
-        this.contents = contents;
+        inventory.setContents(contents);
     }
 
     @Override
     public void load(DataKey key) throws NPCLoadException {
-        contents = parseContents(key);
+        inventory.setContents(parseContents(key));
     }
 
     @Override
     public void save(DataKey key) {
         int slot = 0;
-        for (ItemStack item : contents) {
+        for (ItemStack item : inventory.getContents()) {
             // Clear previous items to avoid conflicts
             key.removeKey(String.valueOf(slot));
             if (item != null)
@@ -61,25 +60,30 @@ public class Inventory extends Trait {
     }
 
     private ItemStack[] parseContents(DataKey key) throws NPCLoadException {
-        ItemStack[] contents = new ItemStack[36];
-        for (DataKey slotKey : key.getIntegerSubKeys())
-            contents[Integer.parseInt(slotKey.name())] = getItemStack(slotKey);
+        ItemStack[] contents = new ItemStack[inventory.getSize()];
+        for (DataKey slotKey : key.getIntegerSubKeys()) {
+            int index = Integer.parseInt(slotKey.name());
+            if (index >= contents.length)
+                continue;
+            contents[index] = getItemStack(slotKey);
+        }
         return contents;
     }
 
     private ItemStack getItemStack(DataKey key) throws NPCLoadException {
         try {
-            ItemStack item = new ItemStack(Material.getMaterial(key.getString("name").toUpperCase().replace('-', '_')),
-                    key.getInt("amount"), (short) key.getLong("data"));
+            ItemStack item = new ItemStack(Material.matchMaterial(key.getString("name")), key.getInt("amount"),
+                    (short) key.getLong("data"));
             if (key.keyExists("enchantments")) {
                 Map<Enchantment, Integer> enchantments = new HashMap<Enchantment, Integer>();
                 for (DataKey subKey : key.getRelative("enchantments").getSubKeys()) {
-                    Enchantment enchantment = Enchantment.getByName(subKey.name().toUpperCase().replace('-', '_'));
-                    if (enchantment != null && enchantment.canEnchantItem(item))
+                    Enchantment enchantment = Enchantment.getByName(subKey.name());
+                    if (enchantment != null && enchantment.canEnchantItem(item)) {
                         enchantments.put(
                                 enchantment,
                                 subKey.getInt("") <= enchantment.getMaxLevel() ? subKey.getInt("") : enchantment
                                         .getMaxLevel());
+                    }
                 }
                 item.addEnchantments(enchantments);
             }
@@ -95,13 +99,12 @@ public class Inventory extends Trait {
         key.setLong("data", item.getDurability());
 
         for (Enchantment enchantment : item.getEnchantments().keySet()) {
-            key.getRelative("enchantments").setInt(enchantment.getName().toLowerCase().replace('_', '-'),
-                    item.getEnchantmentLevel(enchantment));
+            key.getRelative("enchantments").setInt(enchantment.getName(), item.getEnchantmentLevel(enchantment));
         }
     }
 
     @Override
     public String toString() {
-        return "Inventory{" + contents + "}";
+        return "Inventory{" + inventory.getContents() + "}";
     }
 }
