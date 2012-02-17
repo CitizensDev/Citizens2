128b77e35c1f958a94fb9aaff57f29105fb8a081
fullwall <fullwall@optusnet.com>
2012-02-17 15:48:53 +0800

    Fix pom.xml

--- src/net/citizensnpcs/api/util/DatabaseStorage.java
+++ src/net/citizensnpcs/api/util/DatabaseStorage.java
@@ -1,12 +1,51 @@
 package net.citizensnpcs.api.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import org.apache.commons.dbutils.DbUtils;
+
+import com.google.common.collect.Lists;
 
 public class DatabaseStorage implements Storage {
+    private Connection conn;
+    private final List<String> tables = Lists.newArrayList();
+    private final String url, username, password;
+    private final DatabaseType type;
+
+    public DatabaseStorage(String driver, String url, String username, String password) throws SQLException {
+        url = "jdbc:" + url;
+        this.url = url;
+        this.username = username;
+        this.password = password;
+        this.type = DatabaseType.match(driver);
+        type.load();
+        conn = DriverManager.getConnection(url, username, password);
+        ResultSet table = conn.getMetaData().getTables(null, null, null, new String[] { "TABLE" });
+        while (table.next()) {
+            tables.add(table.getString(3));
+        }
+        table.close();
+    }
+
+    private void ensureConnection() throws SQLException {
+        if (type == DatabaseType.SQLITE)
+            return;
+
+        if (!conn.isValid(0)) {
+            conn = (username.isEmpty() && password.isEmpty()) ? DriverManager.getConnection(url) : DriverManager
+                    .getConnection(url, username, password);
+        }
+    }
 
     @Override
     public DataKey getKey(String root) {
+        for (String split : root.split("\\.")) {
+
+        }
         // TODO Auto-generated method stub
         return null;
     }
@@ -137,6 +176,36 @@ public class DatabaseStorage implements Storage {
         public void setString(String key, String value) {
             // TODO Auto-generated method stub
 
+        }
+    }
+
+    public enum DatabaseType {
+        MYSQL("com.mysql.jdbc.Driver"),
+        H2("org.h2.Driver"),
+        POSTGRE("org.postgresql.Driver"),
+        SQLITE("org.sqlite.JDBC");
+        private String driver;
+        private boolean loaded = false;
+
+        DatabaseType(String driver) {
+            this.driver = driver;
+        }
+
+        public static DatabaseType match(String driver) {
+            for (DatabaseType type : DatabaseType.values()) {
+                if (type.name().toLowerCase().contains(driver)) {
+                    return type;
+                }
+            }
+            return null;
+        }
+
+        public boolean load() {
+            if (loaded)
+                return true;
+            if (DbUtils.loadDriver(driver))
+                loaded = true;
+            return loaded;
         }
     }
 }
