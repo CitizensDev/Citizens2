c77d5cb431945b5b5714dafeb7b034a740eedd5a
aPunch <alexpuncochar@yahoo.com>
2012-02-27 04:59:33 -0600

    added reload command - do not use /reload

--- src/main/java/net/citizensnpcs/command/command/AdminCommands.java
+++ src/main/java/net/citizensnpcs/command/command/AdminCommands.java
@@ -1,12 +1,15 @@
 package net.citizensnpcs.command.command;
 
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import net.citizensnpcs.Citizens;
+import net.citizensnpcs.api.exception.NPCLoadException;
 import net.citizensnpcs.api.npc.NPC;
 import net.citizensnpcs.command.Command;
 import net.citizensnpcs.command.CommandContext;
 import net.citizensnpcs.command.Requirements;
+import net.citizensnpcs.command.ServerCommand;
 import net.citizensnpcs.util.Messaging;
 import net.citizensnpcs.util.StringHelper;
 
@@ -18,16 +21,33 @@ public class AdminCommands {
         this.plugin = plugin;
     }
 
-    @Command(
-             aliases = { "citizens" },
-             desc = "Shows basic plugin information",
-             max = 0,
-             permission = "admin")
-    @Requirements
+    @Command(aliases = { "citizens" }, desc = "Show basic plugin information", max = 0, permission = "admin")
     public void citizens(CommandContext args, Player player, NPC npc) {
-        Messaging.send(player, "          " + StringHelper.wrapHeader("<e>Citizens v" + plugin.getDescription().getVersion()));
+        Messaging.send(player, "          "
+                + StringHelper.wrapHeader("<e>Citizens v" + plugin.getDescription().getVersion()));
         Messaging.send(player, "     <7>-- <c>Written by fullwall and aPunch");
         Messaging.send(player, "     <7>-- <c>Source: http://github.com/CitizensDev");
         Messaging.send(player, "     <7>-- <c>Website: " + plugin.getDescription().getWebsite());
+    }
+
+    @Command(
+             aliases = { "citizens" },
+             usage = "reload",
+             desc = "Reload Citizens",
+             modifiers = { "reload" },
+             min = 1,
+             max = 1,
+             permission = "admin")
+    @ServerCommand
+    public void reload(CommandContext args, CommandSender sender, NPC npc) {
+        // TODO possibly could be made more safe
+        Messaging.send(sender, "<e>Reloading Citizens...");
+        try {
+            plugin.reload();
+            Messaging.send(sender, "<e>Citizens reloaded.");
+        } catch (NPCLoadException e) {
+            Messaging.sendError(sender, "Error occured while reloading, see console.");
+            e.printStackTrace();
+        }
     }
 }
