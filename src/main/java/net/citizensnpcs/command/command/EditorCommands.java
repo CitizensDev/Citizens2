acf0fe15b13a13406b1d9fed83b59bcf1523519f
aPunch <alexpuncochar@yahoo.com>
2012-02-27 02:32:43 -0600

    added mob type requirement to commands

--- src/main/java/net/citizensnpcs/command/command/EditorCommands.java
+++ src/main/java/net/citizensnpcs/command/command/EditorCommands.java
@@ -1,12 +1,15 @@
 package net.citizensnpcs.command.command;
 
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
 import net.citizensnpcs.Citizens;
 import net.citizensnpcs.api.npc.NPC;
 import net.citizensnpcs.command.Command;
 import net.citizensnpcs.command.CommandContext;
+import net.citizensnpcs.command.Requirements;
 
+@Requirements(selected = true, ownership = true)
 public class EditorCommands {
 
     public EditorCommands(Citizens plugin) {
@@ -14,11 +17,35 @@ public class EditorCommands {
 
     @Command(
              aliases = { "npc" },
-             usage = "edit (editor)",
-             desc = "Toggle an NPC editor",
-             modifiers = { "edit" },
-             min = 2,
-             max = 2)
-    public void toggleEquipEditor(CommandContext args, Player player, NPC npc) {
+             usage = "equip",
+             desc = "Toggle the equipment editor",
+             modifiers = { "equip" },
+             min = 1,
+             max = 1,
+             permission = "npc.edit.equip")
+    @Requirements(selected = true, ownership = true, type = EntityType.PLAYER)
+    public void equip(CommandContext args, Player player, NPC npc) {
+    }
+
+    @Command(
+             aliases = { "npc" },
+             usage = "path",
+             desc = "Toggle the waypoint editor",
+             modifiers = { "path", "waypoints" },
+             min = 1,
+             max = 1,
+             permission = "npc.edit.path")
+    public void path(CommandContext args, Player player, NPC npc) {
+    }
+
+    @Command(
+             aliases = { "npc" },
+             usage = "text",
+             desc = "Toggle the text editor",
+             modifiers = { "text" },
+             min = 1,
+             max = 1,
+             permission = "npc.edit.text")
+    public void text(CommandContext args, Player player, NPC npc) {
     }
 }
