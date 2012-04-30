154192ae38e928bf247bc8794e0f772eac43b7ac
fullwall <fullwall@optusnet.com>
2012-04-30 23:19:07 +0800

    Set the head yaw as well, remove bukkit from pom as it is included in craftbukkit

--- src/main/java/net/citizensnpcs/npc/entity/CitizensHumanNPC.java
+++ src/main/java/net/citizensnpcs/npc/entity/CitizensHumanNPC.java
@@ -28,6 +28,7 @@ public class CitizensHumanNPC extends CitizensNPC implements Equipable {
         EntityHumanNPC handle = new EntityHumanNPC(ws.getServer().getServer(), ws,
                 StringHelper.parseColors(getFullName()), new ItemInWorldManager(ws), this);
         handle.setPositionRotation(loc.getX(), loc.getY(), loc.getZ(), loc.getYaw(), loc.getPitch());
+        handle.X = loc.getYaw();
         return handle;
     }
 
