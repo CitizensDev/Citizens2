a0192cdb4b87cb4ffcb04e882cdb516b9fa789b2
fullwall <fullwall25@gmail.com>
2012-09-06 17:34:52 +0800

    Change pom.xml, stuckaction

--- src/main/java/net/citizensnpcs/api/ai/TeleportStuckAction.java
+++ src/main/java/net/citizensnpcs/api/ai/TeleportStuckAction.java
@@ -2,6 +2,9 @@ package net.citizensnpcs.api.ai;
 
 import net.citizensnpcs.api.npc.NPC;
 
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+
 public class TeleportStuckAction implements StuckAction {
     private TeleportStuckAction() {
         // singleton
@@ -9,10 +12,18 @@ public class TeleportStuckAction implements StuckAction {
 
     @Override
     public void run(NPC npc, Navigator navigator) {
-        if (!npc.isSpawned() || navigator.getTargetType() != TargetType.LOCATION)
+        if (!npc.isSpawned())
             return;
-        npc.getBukkitEntity().teleport(navigator.getTargetAsLocation());
+        Block block = navigator.getTargetAsLocation().getBlock();
+        int iterations = 0;
+        while (!block.isEmpty()) {
+            block = block.getRelative(BlockFace.UP);
+            if (++iterations >= MAX_ITERATIONS && !block.isEmpty())
+                block = navigator.getTargetAsLocation().getBlock();
+        }
+        npc.getBukkitEntity().teleport(block.getLocation());
     }
 
+    private static int MAX_ITERATIONS = 10;
     public static TeleportStuckAction INSTANCE = new TeleportStuckAction();
 }
