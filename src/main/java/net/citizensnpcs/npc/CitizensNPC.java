d59f3503190d748ed4bfb940b0dc1bffe958a838
fullwall <fullwall25@gmail.com>
2012-09-12 17:06:21 +0800

    Removed NPC metadata for checking that an entity is an NPC; Bukkit will not be fixing this per BUKKIT-2501

--- src/main/java/net/citizensnpcs/npc/CitizensNPC.java
+++ src/main/java/net/citizensnpcs/npc/CitizensNPC.java
@@ -19,7 +19,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
-import org.bukkit.metadata.FixedMetadataValue;
 
 public abstract class CitizensNPC extends AbstractNPC {
     protected EntityLiving mcEntity;
@@ -141,8 +140,6 @@ public abstract class CitizensNPC extends AbstractNPC {
 
         mcEntity.world.addEntity(mcEntity, SpawnReason.CUSTOM);
         mcEntity.world.players.remove(mcEntity);
-        getBukkitEntity().setMetadata(NPC_METADATA_MARKER,
-                new FixedMetadataValue(CitizensAPI.getPlugin(), true));
 
         // Set the spawned state
         getTrait(CurrentLocation.class).setLocation(loc);
@@ -166,6 +163,4 @@ public abstract class CitizensNPC extends AbstractNPC {
             ex.printStackTrace();
         }
     }
-
-    private static final String NPC_METADATA_MARKER = "NPC";
 }
