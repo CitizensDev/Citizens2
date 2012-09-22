a173866ff0355774034c2617c5683f1c1ce8c5be
fullwall <fullwall25@gmail.com>
2012-09-22 16:09:57 +0800

    Accept more blocks as empty in the default stuck action

--- src/main/java/net/citizensnpcs/api/ai/TeleportStuckAction.java
+++ src/main/java/net/citizensnpcs/api/ai/TeleportStuckAction.java
@@ -1,8 +1,12 @@
 package net.citizensnpcs.api.ai;
 
+import java.util.EnumSet;
+import java.util.Set;
+
 import net.citizensnpcs.api.npc.NPC;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 
@@ -20,9 +24,9 @@ public class TeleportStuckAction implements StuckAction {
             return true;
         Block block = base.getBlock();
         int iterations = 0;
-        while (!block.isEmpty()) {
+        while (!EMPTY_BLOCKS.contains(block.getType())) {
             block = block.getRelative(BlockFace.UP);
-            if (++iterations >= MAX_ITERATIONS && !block.isEmpty())
+            if (++iterations >= MAX_ITERATIONS)
                 block = base.getBlock();
         }
         npc.getBukkitEntity().teleport(block.getLocation());
@@ -30,7 +34,14 @@ public class TeleportStuckAction implements StuckAction {
     }
 
     public static TeleportStuckAction INSTANCE = new TeleportStuckAction();
-
+    private static Set<Material> EMPTY_BLOCKS = EnumSet.of(Material.AIR, Material.BIRCH_WOOD_STAIRS,
+            Material.BRICK_STAIRS, Material.CAKE_BLOCK, Material.COBBLESTONE_STAIRS, Material.DEAD_BUSH,
+            Material.JUNGLE_WOOD_STAIRS, Material.LADDER, Material.LONG_GRASS, Material.RAILS,
+            Material.POWERED_RAIL, Material.REDSTONE_WIRE, Material.REDSTONE_TORCH_ON,
+            Material.REDSTONE_TORCH_OFF, Material.TRAP_DOOR, Material.TRIPWIRE, Material.TRIPWIRE_HOOK,
+            Material.VINE, Material.WALL_SIGN, Material.WATER, Material.WOOD_STAIRS, Material.WOOD_STEP,
+            Material.WOODEN_DOOR, Material.YELLOW_FLOWER, Material.RED_ROSE, Material.SIGN,
+            Material.SIGN_POST);
     private static int MAX_ITERATIONS = 10;
     private static final double RANGE = 10;
 }
