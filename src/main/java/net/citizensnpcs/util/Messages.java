fda2ea4cadc46128aaa08923fc08da99cd454851
fullwall <fullwall25@gmail.com>
2012-10-05 20:15:06 +0800

    Make parent folders before saving

--- src/main/java/net/citizensnpcs/util/Messages.java
+++ src/main/java/net/citizensnpcs/util/Messages.java
@@ -85,6 +85,7 @@ public class Messages {
     public static final String METRICS_ERROR_NOTIFICATION = "citizens.notifications.metrics-load-error";
     public static final String METRICS_NOTIFICATION = "citizens.notifications.metrics-started";
     public static final String MINIMUM_COST_REQUIRED = "citizens.economy.minimum-cost-required";
+    public static final String MISSING_TRANSLATIONS = "citizens.notifications.missing-translations";
     public static final String MOBTYPE_CANNOT_BE_AGED = "citizens.commands.npc.age.cannot-be-aged";
     public static final String MONEY_WITHDRAWN = "citizens.economy.money-withdrawn";
     public static final String NO_NPC_WITH_ID_FOUND = "citizens.commands.npc.spawn.missing-npc-id";
@@ -152,7 +153,6 @@ public class Messages {
     public static final String VULNERABLE_STOPPED = "citizens.commands.npc.vulnerable.stopped";
     public static final String WAYPOINT_PROVIDER_SET = "citizens.waypoints.set-provider";
     public static final String WRITING_DEFAULT_SETTING = "citizens.settings.writing-default";
-    public static final String MISSING_TRANSLATIONS = "citizens.notifications.missing-translations";
 
     private static Properties getDefaultBundleProperties() {
         Properties defaults = new Properties();
