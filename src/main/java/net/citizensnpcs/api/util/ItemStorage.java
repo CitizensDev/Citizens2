3f7dc5ea6ffb3c1075cd54f77c1227e4904eb775
fullwall <fullwall25@gmail.com>
2012-12-22 16:58:29 +0800

    Add Firework (de)serialisation

--- src/main/java/net/citizensnpcs/api/util/ItemStorage.java
+++ src/main/java/net/citizensnpcs/api/util/ItemStorage.java
@@ -6,10 +6,12 @@ import java.util.Map;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.inventory.meta.MapMeta;
@@ -21,6 +23,36 @@ import org.bukkit.potion.PotionEffectType;
 import com.google.common.collect.Lists;
 
 public class ItemStorage {
+    private static Iterable<Color> deserialiseColors(DataKey key) {
+        List<Color> colors = Lists.newArrayList();
+        for (DataKey sub : key.getIntegerSubKeys()) {
+            colors.add(Color.fromRGB(sub.getInt("rgb")));
+        }
+        return colors;
+    }
+
+    private static Map<Enchantment, Integer> deserialiseEnchantments(DataKey root, ItemStack res) {
+        Map<Enchantment, Integer> enchantments = new HashMap<Enchantment, Integer>();
+        for (DataKey subKey : root.getSubKeys()) {
+            Enchantment enchantment = Enchantment.getById(Integer.parseInt(subKey.name()));
+            if (enchantment != null && enchantment.canEnchantItem(res)) {
+                int level = Math.min(subKey.getInt(""), enchantment.getMaxLevel());
+                enchantments.put(enchantment, level);
+            }
+        }
+        return enchantments;
+    }
+
+    private static FireworkEffect deserialiseFireworkEffect(DataKey key) {
+        FireworkEffect.Builder effect = FireworkEffect.builder();
+        effect.flicker(key.getBoolean("flicker"));
+        effect.trail(key.getBoolean("trail"));
+        effect.with(FireworkEffect.Type.valueOf(key.getString("type")));
+        effect.withColor(deserialiseColors(key.getRelative("colors")));
+        effect.withFade(deserialiseColors(key.getRelative("fadecolors")));
+        return effect.build();
+    }
+
     private static void deserialiseMeta(DataKey root, ItemStack res) {
         if (root.keyExists("lore")) {
             ItemMeta meta = ensureMeta(res);
@@ -33,6 +65,14 @@ public class ItemStorage {
         if (root.keyExists("displayname")) {
             ItemMeta meta = ensureMeta(res);
             meta.setDisplayName(root.getString("displayname"));
+            res.setItemMeta(meta);
+        }
+        if (root.keyExists("firework")) {
+            FireworkMeta meta = ensureMeta(res);
+            for (DataKey sub : root.getRelative("firework.effects").getIntegerSubKeys()) {
+                meta.addEffect(deserialiseFireworkEffect(sub));
+            }
+            meta.setPower(root.getInt("firework.power"));
             res.setItemMeta(meta);
         }
         if (root.keyExists("book")) {
@@ -91,14 +131,8 @@ public class ItemStorage {
         }
 
         if (root.keyExists("enchantments")) {
-            Map<Enchantment, Integer> enchantments = new HashMap<Enchantment, Integer>();
-            for (DataKey subKey : root.getRelative("enchantments").getSubKeys()) {
-                Enchantment enchantment = Enchantment.getById(Integer.parseInt(subKey.name()));
-                if (enchantment != null && enchantment.canEnchantItem(res)) {
-                    int level = Math.min(subKey.getInt(""), enchantment.getMaxLevel());
-                    enchantments.put(enchantment, level);
-                }
-            }
+            Map<Enchantment, Integer> enchantments = deserialiseEnchantments(
+                    root.getRelative("enchantments"), res);
             res.addEnchantments(enchantments);
         }
         deserialiseMeta(root.getRelative("meta"), res);
@@ -127,10 +161,27 @@ public class ItemStorage {
             serialiseMeta(key.getRelative("meta"), meta);
         } else
             key.removeKey("meta");
+        serialiseEnchantments(key.getRelative("enchantments"), item.getEnchantments());
+    }
 
-        key = key.getRelative("enchantments");
-        for (Enchantment enchantment : item.getEnchantments().keySet())
-            key.setInt(Integer.toString(enchantment.getId()), item.getEnchantmentLevel(enchantment));
+    private static void serialiseColors(DataKey key, List<Color> colors) {
+        for (int i = 0; i < colors.size(); i++) {
+            Color color = colors.get(i);
+            key.getRelative(i).setInt("rgb", color.asRGB());
+        }
+    }
+
+    private static void serialiseEnchantments(DataKey key, Map<Enchantment, Integer> enchantments) {
+        for (Map.Entry<Enchantment, Integer> enchantment : enchantments.entrySet())
+            key.setInt(Integer.toString(enchantment.getKey().getId()), enchantment.getValue());
+    }
+
+    private static void serialiseFireworkEffect(DataKey key, FireworkEffect effect) {
+        key.setBoolean("trail", effect.hasTrail());
+        key.setBoolean("flicker", effect.hasFlicker());
+        key.setString("type", effect.getType().name());
+        serialiseColors(key.getRelative("colors"), effect.getColors());
+        serialiseColors(key.getRelative("fadecolors"), effect.getFadeColors());
     }
 
     private static void serialiseMeta(DataKey key, ItemMeta meta) {
@@ -155,6 +206,7 @@ public class ItemStorage {
             }
             key.setString("book.title", book.getTitle());
             key.setString("book.author", book.getAuthor());
+            serialiseEnchantments(key.getRelative("book.enchantments"), book.getEnchants());
         } else
             key.removeKey("book");
 
@@ -163,6 +215,17 @@ public class ItemStorage {
             key.setString("skull.owner", skull.getOwner());
         } else
             key.removeKey("skull");
+
+        if (meta instanceof FireworkMeta) {
+            FireworkMeta firework = (FireworkMeta) meta;
+            int i = 0;
+            for (FireworkEffect effect : firework.getEffects()) {
+                serialiseFireworkEffect(key.getRelative("firework.effects." + i), effect);
+                i++;
+            }
+            key.setInt("firework.power", firework.getPower());
+        } else
+            key.removeKey("firework");
 
         if (meta instanceof MapMeta) {
             MapMeta map = (MapMeta) meta;
