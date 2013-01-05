4f85b0b90a984f3cde5c05da270ec00f7cf8ba5a
fullwall <fullwall25@gmail.com>
2013-01-05 23:48:37 +0800

    Add tryCreateInstance and withSupplier

--- src/main/java/net/citizensnpcs/api/trait/TraitInfo.java
+++ src/main/java/net/citizensnpcs/api/trait/TraitInfo.java
@@ -1,10 +1,14 @@
 package net.citizensnpcs.api.trait;
 
+import com.google.common.base.Preconditions;
+import com.google.common.base.Supplier;
+
 /**
  * Builds a trait.
  */
 public final class TraitInfo {
     private String name;
+    private Supplier<? extends Trait> supplier;
     private final Class<? extends Trait> trait;
 
     private TraitInfo(Class<? extends Trait> trait) {
@@ -19,8 +23,26 @@ public final class TraitInfo {
         return name;
     }
 
+    @SuppressWarnings("unchecked")
+    public <T extends Trait> T tryCreateInstance() {
+        if (supplier != null)
+            return (T) supplier.get();
+        try {
+            return (T) trait.newInstance();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
     public TraitInfo withName(String name) {
+        Preconditions.checkNotNull(name);
         this.name = name.toLowerCase();
+        return this;
+    }
+
+    public TraitInfo withSupplier(Supplier<? extends Trait> supplier) {
+        this.supplier = supplier;
         return this;
     }
 
@@ -35,6 +57,7 @@ public final class TraitInfo {
      *             If the trait class does not have a no-arguments constructor
      */
     public static TraitInfo create(Class<? extends Trait> trait) {
+        Preconditions.checkNotNull(trait);
         try {
             trait.getConstructor(new Class<?>[] {});
         } catch (NoSuchMethodException e) {
