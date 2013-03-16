630f97074cebff60ad902384e98e7bcc75654c59
agentkid20@gmail.com <agentkid20@gmail.com>
2013-03-15 23:17:50 -0400

    Attempt fix for bad CB version number.

--- src/main/java/net/citizensnpcs/Citizens.java
+++ src/main/java/net/citizensnpcs/Citizens.java
@@ -434,5 +434,5 @@ public class Citizens extends JavaPlugin implements CitizensPlugin {
         return false;
     }
 
-    private static final String COMPATIBLE_MC_VERSION = "1.4.7";
+    private static final String COMPATIBLE_MC_VERSION = "1.5";
 }
