7b949c4706d46a834dc95f832e2469be6c5bba4f
fullwall <fullwall25@gmail.com>
2013-07-03 16:44:33 +0800

    Test for wolv

--- src/main/java/org/bukkit/craftbukkit/v1_6_R1/entity/CraftEntity.java
+++
@@ -1,9 +0,0 @@
-package org.bukkit.craftbukkit.v1_6_R1.entity;
-
-import org.bukkit.entity.Entity;
-
-public abstract class CraftEntity implements Entity {
-    public net.minecraft.server.v1_6_R1.Entity getHandle() {
-        return null;
-    }
-}
