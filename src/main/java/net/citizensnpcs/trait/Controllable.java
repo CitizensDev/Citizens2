c4703e1f62ee590c5fec950c69a33d97e946d7bd
fullwall <fullwall25@gmail.com>
2013-08-13 12:20:57 +0800

    Call NPCVehicleExitEvent for controllables, possibly fix a despawn/respawn bug

--- src/main/java/net/citizensnpcs/trait/Controllable.java
+++ src/main/java/net/citizensnpcs/trait/Controllable.java
@@ -7,6 +7,7 @@ import net.citizensnpcs.Settings.Setting;
 import net.citizensnpcs.api.command.CommandConfigurable;
 import net.citizensnpcs.api.command.CommandContext;
 import net.citizensnpcs.api.event.NPCRightClickEvent;
+import net.citizensnpcs.api.event.NPCVehicleExitEvent;
 import net.citizensnpcs.api.exception.NPCLoadException;
 import net.citizensnpcs.api.persistence.Persist;
 import net.citizensnpcs.api.trait.Trait;
@@ -18,6 +19,7 @@ import net.minecraft.server.v1_6_R2.EntityEnderDragon;
 import net.minecraft.server.v1_6_R2.EntityLiving;
 import net.minecraft.server.v1_6_R2.EntityPlayer;
 
+import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_6_R2.entity.CraftLivingEntity;
 import org.bukkit.craftbukkit.v1_6_R2.entity.CraftPlayer;
 import org.bukkit.entity.Entity;
@@ -65,12 +67,15 @@ public class Controllable extends Trait implements Toggleable, CommandConfigurab
     private void enterOrLeaveVehicle(Player player) {
         EntityPlayer handle = ((CraftPlayer) player).getHandle();
         if (getHandle().passenger != null) {
-            if (getHandle().passenger == handle)
+            if (getHandle().passenger == handle) {
                 player.leaveVehicle();
+                Bukkit.getPluginManager().callEvent(new NPCVehicleExitEvent(npc, player));
+            }
             return;
         }
-        if (npc.getTrait(Owner.class).isOwnedBy(handle.getBukkitEntity()))
+        if (npc.getTrait(Owner.class).isOwnedBy(handle.getBukkitEntity())) {
             handle.setPassengerOf(getHandle());
+        }
     }
 
     private EntityLiving getHandle() {
