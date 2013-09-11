815733c2ad39df07b9fd3a3d03e2bca7e1a83511
fullwall <fullwall25@gmail.com>
2013-09-11 22:27:24 +0800

    Breaking change: add new registry param to CitizensNPC constructor

--- src/main/java/net/citizensnpcs/npc/entity/BlazeController.java
+++ src/main/java/net/citizensnpcs/npc/entity/BlazeController.java
@@ -84,8 +84,9 @@ public class BlazeController extends MobEntityController {
             // this method is called by both the entities involved - cancelling
             // it will not stop the NPC from moving.
             super.collide(entity);
-            if (npc != null)
+            if (npc != null) {
                 Util.callCollisionEvent(npc, entity.getBukkitEntity());
+            }
         }
 
         @Override
