e06ed6f60ed171036098f39b524fb6c9e29db370
fullwall <fullwall25@gmail.com>
2014-01-11 23:19:23 +0800

    Use Persistable in WaypointProvider

--- src/main/java/net/citizensnpcs/trait/waypoint/WaypointProvider.java
+++ src/main/java/net/citizensnpcs/trait/waypoint/WaypointProvider.java
@@ -2,15 +2,14 @@ package net.citizensnpcs.trait.waypoint;
 
 import net.citizensnpcs.api.command.CommandContext;
 import net.citizensnpcs.api.npc.NPC;
-import net.citizensnpcs.api.util.DataKey;
+import net.citizensnpcs.api.persistence.Persistable;
 
 import org.bukkit.entity.Player;
 
-public interface WaypointProvider {
-
+public interface WaypointProvider extends Persistable {
     /**
      * Creates an {@link WaypointEditor} with the given {@link Player}.
-     * 
+     *
      * @param player
      *            The player to link the editor with
      * @param args
@@ -20,18 +19,10 @@ public interface WaypointProvider {
 
     /**
      * Returns whether this provider has paused execution of waypoints.
-     * 
+     *
      * @return Whether the provider is paused.
      */
     public boolean isPaused();
-
-    /**
-     * Loads from the specified {@link DataKey}.
-     * 
-     * @param key
-     *            The key to load from
-     */
-    public void load(DataKey key);
 
     /**
      * Called when the provider is removed from the NPC.
@@ -40,23 +31,15 @@ public interface WaypointProvider {
 
     /**
      * Called when the {@link NPC} attached to this provider is spawned.
-     * 
+     *
      * @param npc
      *            The attached NPC
      */
     public void onSpawn(NPC npc);
 
     /**
-     * Saves to the specified {@link DataKey}.
-     * 
-     * @param key
-     *            The key to save to
-     */
-    public void save(DataKey key);
-
-    /**
      * Pauses waypoint execution.
-     * 
+     *
      * @param paused
      *            Whether to pause waypoint execution.
      */
