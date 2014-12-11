2ecddf6f87d8192a6d2104de1c213ff1fdc3426f
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2014-12-11 15:56:10 -0800

    Improve NPC pseudo-respawning
    In initial testing, longer periods were better. However, that was before
    a lot of recoding... shorter periods are now superior and keeping
    significantly more NPCs visible (When dealing with batches so large that
    the system fails to load them all)

--- src/main/java/net/citizensnpcs/npc/entity/HumanController.java
+++ src/main/java/net/citizensnpcs/npc/entity/HumanController.java
@@ -93,7 +93,7 @@ public class HumanController extends AbstractEntityController {
                     NMS.sendPlayerlistPacket(false, null, npc);
                 }
             }
-        }, 60);
+        }, 2);
         return handle.getBukkitEntity();
     }
 
