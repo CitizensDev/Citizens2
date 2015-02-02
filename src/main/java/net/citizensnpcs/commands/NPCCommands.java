8698312a87719a76c10e6afa3eef2c0b909b96b2
sanjay900 <sanjay.govind9@gmail.com>
2015-02-03 10:52:52 +1300

    Support setting permanent skins
    A permanent skin is set with ./npc skin -p name and will NOT change when
    that player changes their skin, opening up lots more skin options.
    To do this with code, set
    npc.data().setPersistent(NPC.PLAYER_SKIN_UUID_METADATA, to the players
    UUID/ Name and then
    npc.data().setPersistent(NPC.PLAYER_SKIN_TEXTURE_PROPERTIES, "cache");
    to tell the plugin to keep a permanent cache of their skin. Due to how
    skins are implemented, these skins will work forever.

--- src/main/java/net/citizensnpcs/commands/NPCCommands.java
+++ src/main/java/net/citizensnpcs/commands/NPCCommands.java
@@ -1229,11 +1229,12 @@ public class NPCCommands {
 
     @Command(
             aliases = { "npc" },
-            usage = "skin (-c) [name]",
-            desc = "Sets an NPC's skin name",
+            usage = "skin (-c,b) [name]",
+            desc = "Sets an NPC's skin name, Use -b to save a skin snapshot that won't change",
             modifiers = { "skin" },
             min = 1,
             max = 2,
+            flags = "cp",
             permission = "citizens.npc.skin")
     @Requirements(types = EntityType.PLAYER, selected = true, ownership = true)
     public void skin(final CommandContext args, final CommandSender sender, final NPC npc) throws CommandException {
@@ -1244,6 +1245,9 @@ public class NPCCommands {
             if (args.argsLength() != 2)
                 throw new CommandException();
             npc.data().setPersistent(NPC.PLAYER_SKIN_UUID_METADATA, args.getString(1));
+            if (args.hasFlag('p')) {
+                npc.data().setPersistent(NPC.PLAYER_SKIN_TEXTURE_PROPERTIES, "cache");
+            }
             skinName = args.getString(1);
         }
         Messaging.sendTr(sender, Messages.SKIN_SET, npc.getName(), skinName);
