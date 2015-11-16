1216679b41aadfe6622fb98f1811368c1d44c42d
fullwall <fullwall25@gmail.com>
2015-11-16 21:36:00 +0800

    Add /npc wither --charged

---
+++ src/main/java/net/citizensnpcs/trait/WitherTrait.java
@@ -0,0 +1,38 @@
+package net.citizensnpcs.trait;
+
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftWither;
+import org.bukkit.entity.Wither;
+
+import net.citizensnpcs.api.persistence.Persist;
+import net.citizensnpcs.api.trait.Trait;
+import net.minecraft.server.v1_8_R3.EntityWither;
+
+public class WitherTrait extends Trait {
+    @Persist("charged")
+    private boolean charged = false;
+
+    public WitherTrait() {
+        super("withertrait");
+    }
+
+    public boolean isCharged() {
+        return charged;
+    }
+
+    @Override
+    public void onSpawn() {
+    }
+
+    @Override
+    public void run() {
+        if (npc.getEntity() instanceof Wither) {
+            Wither wither = (Wither) npc.getEntity();
+            EntityWither handle = ((CraftWither) wither).getHandle();
+            handle.r(charged ? 20 : 0);
+        }
+    }
+
+    public void setCharged(boolean charged) {
+        this.charged = charged;
+    }
+}
