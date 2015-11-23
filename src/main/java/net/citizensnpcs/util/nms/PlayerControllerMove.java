6cb0056b51924e32d2fe2f65c66ad772096f4da4
fullwall <fullwall25@gmail.com>
2015-11-24 01:17:06 +0800

    Fix rabbit movement

--- src/main/java/net/citizensnpcs/util/nms/PlayerControllerMove.java
+++ src/main/java/net/citizensnpcs/util/nms/PlayerControllerMove.java
@@ -2,8 +2,6 @@ package net.citizensnpcs.util.nms;
 
 import java.util.Random;
 
-import org.bukkit.craftbukkit.v1_8_R3.TrigMath;
-
 import net.citizensnpcs.npc.entity.EntityHumanNPC;
 import net.citizensnpcs.util.NMS;
 import net.minecraft.server.v1_8_R3.AttributeInstance;
@@ -84,24 +82,22 @@ public class PlayerControllerMove extends ControllerMove {
             double d1 = this.d - this.a.locZ;
             double d2 = this.c - i;
             double d3 = d0 * d0 + d2 * d2 + d1 * d1;
-
-            if (d3 >= 2.500000277905201E-007D) {
-                float f = (float) (TrigMath.atan2(d1, d0) * 180.0D / 3.141592741012573D) - 90.0F;
-
-                this.a.yaw = a(this.a.yaw, f, 30.0F);
-                NMS.setHeadYaw(a, this.a.yaw);
-                AttributeInstance speed = this.a.getAttributeInstance(GenericAttributes.MOVEMENT_SPEED);
-                speed.setValue(0.1D * this.e);
-                float movement = (float) (this.e * speed.getValue()) * 10;
-                this.a.ba = movement;
-                if (shouldSlimeJump() || ((d2 > 0.0D) && (d0 * d0 + d1 * d1 < 1.0D))) {
-                    this.h = cg();
-                    this.h /= 3;
-                    if (this.a instanceof EntityHumanNPC) {
-                        ((EntityHumanNPC) this.a).getControllerJump().a();
-                    } else {
-                        ((EntityInsentient) this.a).getControllerJump().a();
-                    }
+            if (d3 < 2.500000277905201E-007D)
+                return;
+            float f = (float) Math.toDegrees(Math.atan2(d1, d0)) - 90.0F;
+            this.a.yaw = a(this.a.yaw, f, 30.0F);
+            NMS.setHeadYaw(a, this.a.yaw);
+            AttributeInstance speed = this.a.getAttributeInstance(GenericAttributes.MOVEMENT_SPEED);
+            speed.setValue(0.1D * this.e);
+            float movement = (float) (this.e * speed.getValue()) * 10;
+            this.a.ba = movement;
+            if (shouldSlimeJump() || ((d2 > 0.0D) && (d0 * d0 + d1 * d1 < 1.0D))) {
+                this.h = cg();
+                this.h /= 3;
+                if (this.a instanceof EntityHumanNPC) {
+                    ((EntityHumanNPC) this.a).getControllerJump().a();
+                } else {
+                    ((EntityInsentient) this.a).getControllerJump().a();
                 }
             }
         }
