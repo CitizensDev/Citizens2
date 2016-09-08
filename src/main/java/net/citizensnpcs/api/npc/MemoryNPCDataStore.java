0af66586ec71de905660fa8d9b1f363dd0798afe
fullwall <fullwall25@gmail.com>
2016-09-09 01:31:48 +0800

    Add MemoryNPCDataStore

---
+++ src/main/java/net/citizensnpcs/api/npc/MemoryNPCDataStore.java
@@ -0,0 +1,34 @@
+package net.citizensnpcs.api.npc;
+
+public class MemoryNPCDataStore implements NPCDataStore {
+    private int lastID;
+
+    @Override
+    public void clearData(NPC npc) {
+    }
+
+    @Override
+    public int createUniqueNPCId(NPCRegistry registry) {
+        return lastID++;
+    }
+
+    @Override
+    public void loadInto(NPCRegistry registry) {
+    }
+
+    @Override
+    public void saveToDisk() {
+    }
+
+    @Override
+    public void saveToDiskImmediate() {
+    }
+
+    @Override
+    public void store(NPC npc) {
+    }
+
+    @Override
+    public void storeAll(NPCRegistry registry) {
+    }
+}
