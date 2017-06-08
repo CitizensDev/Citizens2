4235e72cc101ff6322228bb7efe3d185ecd93d70
fullwall <fullwall25@gmail.com>
2017-06-08 19:37:57 +0800

    Remove subscription to bukkit broadcast

--- v1_11_R1/src/main/java/net/citizensnpcs/nms/v1_11_R1/entity/EntityHumanNPC.java
+++ v1_11_R1/src/main/java/net/citizensnpcs/nms/v1_11_R1/entity/EntityHumanNPC.java
@@ -114,6 +114,7 @@ public class EntityHumanNPC extends EntityPlayer implements NPCHolder, Skinnable
         if (updateCounter + 1 > Setting.PACKET_UPDATE_DELAY.asInt()) {
             updateEffects = true;
         }
+        Bukkit.getServer().getPluginManager().unsubscribeFromPermission("bukkit.broadcast.user", bukkitEntity);
         livingEntityBaseTick();
 
         boolean navigating = npc.getNavigator().isNavigating();
