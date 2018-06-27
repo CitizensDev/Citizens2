42fda25cebfc55a825845ff84dbab7510ec248f6
Alex "mcmonkey" Goodwin <mcmonkey4eva@hotmail.com>
2018-06-26 21:00:03 -0700

    Patch skin command, rename command (#1524)
    * Fix npc skin -t argument order
    
    Command input is doc'd as (and should function as) data then signature, but the called method was signature then data... update calls it in correct order. Also, fixes doc to indicate that a name is valid input, not just uuid (not entirely sure how that is handled when sent out in a packet, but it seems to be correct to feed a name)
    
    * Ensure rename command will handle scoreboard changes
    
    Always update prefix/suffix, not just only when team is newly created
    Guarantee removal entity from scoreboard team on remove
    Also removes a fileheader screwup (first line changed in 1_12_R1 file)
    Also removes non-used code bits (the two coloredName lines - they don't actually functionally do anything)
    Also removes a duplicated data get call
    
    * patch gamemode command
    
    Uppercase + unique permission node
    
    * patch for names of length 30

--- main/src/main/java/net/citizensnpcs/commands/NPCCommands.java
+++ main/src/main/java/net/citizensnpcs/commands/NPCCommands.java
@@ -515,7 +515,7 @@ public class NPCCommands {
             modifiers = { "gamemode" },
             min = 1,
             max = 2,
-            permission = "citizens.npc.gravity")
+            permission = "citizens.npc.gamemode")
     @Requirements(selected = true, ownership = true, types = { EntityType.PLAYER })
     public void gamemode(CommandContext args, CommandSender sender, NPC npc) {
         Player player = (Player) npc.getEntity();
@@ -530,7 +530,7 @@ public class NPCCommands {
             mode = GameMode.getByValue(value);
         } catch (NumberFormatException ex) {
             try {
-                mode = GameMode.valueOf(args.getString(1));
+                mode = GameMode.valueOf(args.getString(1).toUpperCase());
             } catch (IllegalArgumentException e) {
             }
         }
@@ -1459,7 +1459,7 @@ public class NPCCommands {
 
     @Command(
             aliases = { "npc" },
-            usage = "skin (-c -l(atest)) [name] (or -t [uuid] [data] [signature])",
+            usage = "skin (-c -l(atest)) [name] (or -t [uuid/name] [data] [signature])",
             desc = "Sets an NPC's skin name. Use -l to set the skin to always update to the latest",
             modifiers = { "skin" },
             min = 1,
@@ -1479,7 +1479,7 @@ public class NPCCommands {
             if (skinnable == null) {
                 throw new CommandException("Must be spawned.");
             }
-            skinnable.setSkinPersistent(args.getString(1), args.getString(2), args.getString(3));
+            skinnable.setSkinPersistent(args.getString(1), args.getString(3), args.getString(2));
             Messaging.sendTr(sender, Messages.SKIN_SET, npc.getName(), args.getString(1));
             return;
         } else {
