1dc0edd7b082982a0508c64f5b6c806773aea651
fullwall <fullwall25@gmail.com>
2018-09-18 18:22:16 +0800

    Fix compile error

--- v1_8_R3/src/main/java/net/citizensnpcs/nms/v1_8_R3/network/EmptyNetworkManager.java
+++ v1_8_R3/src/main/java/net/citizensnpcs/nms/v1_8_R3/network/EmptyNetworkManager.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import net.citizensnpcs.nms.v1_8_R3.util.NMSImpl;
 import net.minecraft.server.v1_8_R3.EnumProtocolDirection;
 import net.minecraft.server.v1_8_R3.NetworkManager;
-import net.minecraft.server.v1_8_R3.Packet;
 
 public class EmptyNetworkManager extends NetworkManager {
     public EmptyNetworkManager(EnumProtocolDirection flag) throws IOException {
@@ -16,9 +15,5 @@ public class EmptyNetworkManager extends NetworkManager {
     @Override
     public boolean g() {
         return true;
-    }
-
-    @Override
-    public void sendPacket(Packet packet) {
     }
 }
