7b2f08b3722941472c8b1e56607c3f710d0eb837
fullwall <fullwall25@gmail.com>
2019-05-31 00:23:11 +0800

    Update /citizens output

--- main/src/main/java/net/citizensnpcs/commands/AdminCommands.java
+++ main/src/main/java/net/citizensnpcs/commands/AdminCommands.java
@@ -25,9 +25,10 @@ public class AdminCommands {
     public void citizens(CommandContext args, CommandSender sender, NPC npc) throws CommandException {
         Messaging.send(sender,
                 "          " + StringHelper.wrapHeader("<e>Citizens v" + plugin.getDescription().getVersion()));
-        Messaging.send(sender, "     <7>-- <c>Written by fullwall and aPunch");
+        Messaging.send(sender, "     <7>-- <c>Author: fullwall");
         Messaging.send(sender, "     <7>-- <c>Source Code: http://github.com/CitizensDev");
         Messaging.send(sender, "     <7>-- <c>Website: " + plugin.getDescription().getWebsite());
+        Messaging.send(sender, "     <7>-- <c>Report an issue: http://github.com/CitizensDev/Citizens2/issues");
     }
 
     @Command(
