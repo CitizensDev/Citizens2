8843454af03c60692ca7055a0fa02332cfd9b011
fullwall <fullwall25@gmail.com>
2020-02-25 13:35:44 +0800

    Don't reset pitch on older versions of Minecraft

--- v1_8_R3/src/main/java/net/citizensnpcs/nms/v1_8_R3/util/PlayerControllerLook.java
+++ v1_8_R3/src/main/java/net/citizensnpcs/nms/v1_8_R3/util/PlayerControllerLook.java
@@ -21,7 +21,7 @@ public class PlayerControllerLook {
     public void a() {
         if (!NMSImpl.isNavigationFinished(this.a.getNavigation()))
             return;
-        this.a.pitch = 0.0F;
+        // this.a.pitch = 0.0F;
         this.a.aI = this.a.aK;
         if (this.d) {
             this.d = false;
