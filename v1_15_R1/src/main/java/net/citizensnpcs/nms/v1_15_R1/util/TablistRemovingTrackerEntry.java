241810eedc5dc6e0df99ccb27a04ef97844a9787
fullwall <fullwall25@gmail.com>
2020-05-12 23:50:54 +0800

    Delay remove packet one tick

--- v1_15_R1/src/main/java/net/citizensnpcs/nms/v1_15_R1/util/TablistRemovingTrackerEntry.java
+++ v1_15_R1/src/main/java/net/citizensnpcs/nms/v1_15_R1/util/TablistRemovingTrackerEntry.java
@@ -3,9 +3,11 @@ package net.citizensnpcs.nms.v1_15_R1.util;
 import java.util.Set;
 import java.util.function.Consumer;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import net.citizensnpcs.Settings.Setting;
+import net.citizensnpcs.api.CitizensAPI;
 import net.citizensnpcs.util.NMS;
 import net.minecraft.server.v1_15_R1.Entity;
 import net.minecraft.server.v1_15_R1.EntityPlayer;
@@ -33,7 +35,12 @@ public class TablistRemovingTrackerEntry extends EntityTrackerEntry {
         this.tracker.b(entityplayer);
         entityplayer.d(this.tracker);
         if (!tracker.dead && Setting.DISABLE_TABLIST.asBoolean()) {
-            NMS.sendTabListRemove(entityplayer.getBukkitEntity(), (Player) tracker.getBukkitEntity());
+            Bukkit.getScheduler().scheduleSyncDelayedTask(CitizensAPI.getPlugin(), new Runnable() {
+                @Override
+                public void run() {
+                    NMS.sendTabListRemove(entityplayer.getBukkitEntity(), (Player) tracker.getBukkitEntity());
+                }
+            });
         }
     }
 }
