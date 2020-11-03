4542d70a10fd44b68cff48874125e05d652b2bed
fullwall <fullwall25@gmail.com>
2020-11-03 16:15:37 +0800

    Rename to 1_16_R2

---
+++ v1_16_R2/src/main/java/net/citizensnpcs/nms/v1_16_R3/entity/nonliving/EggController.java
@@ -0,0 +1,136 @@
+package net.citizensnpcs.nms.v1_16_R2.entity.nonliving;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_16_R2.CraftServer;
+import org.bukkit.craftbukkit.v1_16_R2.CraftWorld;
+import org.bukkit.craftbukkit.v1_16_R2.entity.CraftEgg;
+import org.bukkit.craftbukkit.v1_16_R2.entity.CraftEntity;
+import org.bukkit.entity.Egg;
+import org.bukkit.entity.Entity;
+import org.bukkit.util.Vector;
+
+import net.citizensnpcs.api.event.NPCPushEvent;
+import net.citizensnpcs.api.npc.NPC;
+import net.citizensnpcs.nms.v1_16_R2.util.NMSImpl;
+import net.citizensnpcs.npc.AbstractEntityController;
+import net.citizensnpcs.npc.CitizensNPC;
+import net.citizensnpcs.npc.ai.NPCHolder;
+import net.citizensnpcs.util.Util;
+import net.minecraft.server.v1_16_R2.EntityEgg;
+import net.minecraft.server.v1_16_R2.EntityTypes;
+import net.minecraft.server.v1_16_R2.NBTTagCompound;
+import net.minecraft.server.v1_16_R2.World;
+import net.minecraft.server.v1_16_R2.WorldServer;
+
+public class EggController extends AbstractEntityController {
+    public EggController() {
+        super(EntityEggNPC.class);
+    }
+
+    @Override
+    protected Entity createEntity(Location at, NPC npc) {
+        WorldServer ws = ((CraftWorld) at.getWorld()).getHandle();
+        final EntityEggNPC handle = new EntityEggNPC(ws, npc, at.getX(), at.getY(), at.getZ());
+        return handle.getBukkitEntity();
+    }
+
+    @Override
+    public Egg getBukkitEntity() {
+        return (Egg) super.getBukkitEntity();
+    }
+
+    public static class EggNPC extends CraftEgg implements NPCHolder {
+        private final CitizensNPC npc;
+
+        public EggNPC(EntityEggNPC entity) {
+            super((CraftServer) Bukkit.getServer(), entity);
+            this.npc = entity.npc;
+        }
+
+        @Override
+        public NPC getNPC() {
+            return npc;
+        }
+    }
+
+    public static class EntityEggNPC extends EntityEgg implements NPCHolder {
+        private final CitizensNPC npc;
+
+        public EntityEggNPC(EntityTypes<? extends EntityEgg> types, World world) {
+            this(types, world, null);
+        }
+
+        public EntityEggNPC(EntityTypes<? extends EntityEgg> types, World world, NPC npc) {
+            super(types, world);
+            this.npc = (CitizensNPC) npc;
+        }
+
+        public EntityEggNPC(World world, NPC npc, double d0, double d1, double d2) {
+            super(world, d0, d1, d2);
+            this.npc = (CitizensNPC) npc;
+        }
+
+        @Override
+        public void collide(net.minecraft.server.v1_16_R2.Entity entity) {
+            // this method is called by both the entities involved - cancelling
+            // it will not stop the NPC from moving.
+            super.collide(entity);
+            if (npc != null) {
+                Util.callCollisionEvent(npc, entity.getBukkitEntity());
+            }
+        }
+
+        @Override
+        public boolean d(NBTTagCompound save) {
+            return npc == null ? super.d(save) : false;
+        }
+
+        @Override
+        public void i(double x, double y, double z) {
+            if (npc == null) {
+                super.i(x, y, z);
+                return;
+            }
+            if (NPCPushEvent.getHandlerList().getRegisteredListeners().length == 0) {
+                if (!npc.data().get(NPC.DEFAULT_PROTECTED_METADATA, true))
+                    super.i(x, y, z);
+                return;
+            }
+            Vector vector = new Vector(x, y, z);
+            NPCPushEvent event = Util.callPushEvent(npc, vector);
+            if (!event.isCancelled()) {
+                vector = event.getCollisionVector();
+                super.i(vector.getX(), vector.getY(), vector.getZ());
+            }
+            // when another entity collides, this method is called to push the
+            // NPC so we prevent it from doing anything if the event is
+            // cancelled.
+        }
+
+        @Override
+        public CraftEntity getBukkitEntity() {
+            if (npc != null && !(super.getBukkitEntity() instanceof NPCHolder)) {
+                NMSImpl.setBukkitEntity(this, new EggNPC(this));
+            }
+            return super.getBukkitEntity();
+        }
+
+        @Override
+        public NPC getNPC() {
+            return npc;
+        }
+
+        @Override
+        public void tick() {
+            if (npc != null) {
+                npc.update();
+                if (!npc.data().get(NPC.DEFAULT_PROTECTED_METADATA, true)) {
+                    super.tick();
+                }
+            } else {
+                super.tick();
+            }
+        }
+    }
+}
