9d8b22b1d4a08f38ad5ed0c11dc6fc31f8c3196d
fullwall <fullwall25@gmail.com>
2020-11-04 16:31:41 +0800

    Add hex colour support &#xxxxxx

--- src/main/java/net/citizensnpcs/api/util/Colorizer.java
+++ src/main/java/net/citizensnpcs/api/util/Colorizer.java
@@ -7,7 +7,9 @@ import org.bukkit.ChatColor;
 
 public class Colorizer {
     public static String parseColors(String parsed) {
-        Matcher matcher = COLOR_MATCHER.matcher(ChatColor.translateAlternateColorCodes('&', parsed));
+        Matcher matcher = HEX_MATCHER.matcher(parsed);
+        parsed = matcher.replaceAll("&x&$1&$2&$3&$4&$5&$6");
+        matcher = COLOR_MATCHER.matcher(ChatColor.translateAlternateColorCodes('&', parsed));
         return matcher.replaceAll(GROUP);
     }
 
@@ -18,11 +20,14 @@ public class Colorizer {
 
     private static Pattern COLOR_MATCHER;
     private static String GROUP = ChatColor.COLOR_CHAR + "$1";
+    private static Pattern HEX_MATCHER = Pattern
+            .compile("&#([0-9a-f])([0-9a-f])([0-9a-f])([0-9a-f])([0-9a-f])([0-9a-f])", Pattern.CASE_INSENSITIVE);
 
     static {
         String colors = "";
-        for (ChatColor color : ChatColor.values())
+        for (ChatColor color : ChatColor.values()) {
             colors += color.getChar();
+        }
         COLOR_MATCHER = Pattern.compile("\\<([COLORS])\\>".replace("COLORS", colors), Pattern.CASE_INSENSITIVE);
     }
 }
