8e31baf2022fd2a1ecdcf027448f3c827b04fc6d
fullwall <fullwall25@gmail.com>
2021-01-25 23:47:43 +0800

    Edit equipment GUI

--- main/src/main/java/net/citizensnpcs/editor/EquipmentGUI.java
+++ main/src/main/java/net/citizensnpcs/editor/EquipmentGUI.java
@@ -2,6 +2,7 @@ package net.citizensnpcs.editor;
 
 import org.bukkit.Material;
 import org.bukkit.entity.HumanEntity;
+import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
 
@@ -12,11 +13,13 @@ import net.citizensnpcs.api.gui.MenuContext;
 import net.citizensnpcs.api.gui.MenuSlot;
 import net.citizensnpcs.api.util.Messaging;
 
-@Menu(title = "NPC Equipment", type = InventoryType.CHEST, dimensions = { 3, 3 })
+@Menu(title = "NPC Equipment", type = InventoryType.CHEST, dimensions = { 2, 5 })
+@MenuSlot(value = { 0, 0 }, material = Material.DIAMOND_SWORD, amount = 1, filter = ClickType.UNKNOWN)
+@MenuSlot(value = { 0, 1 }, material = Material.DIAMOND_HELMET, amount = 1, filter = ClickType.UNKNOWN)
+@MenuSlot(value = { 0, 2 }, material = Material.DIAMOND_CHESTPLATE, amount = 1, filter = ClickType.UNKNOWN)
+@MenuSlot(value = { 0, 3 }, material = Material.DIAMOND_LEGGINGS, amount = 1, filter = ClickType.UNKNOWN)
+@MenuSlot(value = { 0, 4 }, material = Material.DIAMOND_BOOTS, amount = 1, filter = ClickType.UNKNOWN)
 public class EquipmentGUI extends InventoryMenuPage {
-    @MenuSlot(value = { 0, 1 }, material = Material.DIAMOND_SWORD, amount = 1)
-    private InventoryMenuSlot slot;
-
     @Override
     public void create(MenuContext ctx) {
     }
