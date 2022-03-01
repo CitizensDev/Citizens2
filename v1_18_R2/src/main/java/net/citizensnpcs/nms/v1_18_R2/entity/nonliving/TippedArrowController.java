0a9176836987a8ca7ff059a47b1bebfe4fe71fcf
fullwall <fullwall25@gmail.com>
2022-03-01 20:54:08 +0800

    Update for 1.18.2

---
+++ v1_18_R2/src/main/java/net/citizensnpcs/nms/v1_18_R2/entity/nonliving/TippedArrowController.java
@@ -0,0 +1,104 @@
+package net.citizensnpcs.nms.v1_18_R2.entity.nonliving;
+
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_18_R2.CraftServer;
+import org.bukkit.craftbukkit.v1_18_R2.entity.CraftEntity;
+import org.bukkit.craftbukkit.v1_18_R2.entity.CraftTippedArrow;
+import org.bukkit.util.Vector;
+
+import net.citizensnpcs.api.npc.NPC;
+import net.citizensnpcs.nms.v1_18_R2.entity.MobEntityController;
+import net.citizensnpcs.nms.v1_18_R2.util.ForwardingNPCHolder;
+import net.citizensnpcs.nms.v1_18_R2.util.NMSImpl;
+import net.citizensnpcs.npc.CitizensNPC;
+import net.citizensnpcs.npc.ai.NPCHolder;
+import net.citizensnpcs.util.Util;
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.projectile.Arrow;
+import net.minecraft.world.level.Level;
+
+public class TippedArrowController extends MobEntityController {
+    public TippedArrowController() {
+        super(EntityTippedArrowNPC.class);
+    }
+
+    @Override
+    public org.bukkit.entity.Arrow getBukkitEntity() {
+        return (org.bukkit.entity.Arrow) super.getBukkitEntity();
+    }
+
+    public static class EntityTippedArrowNPC extends Arrow implements NPCHolder {
+        private final CitizensNPC npc;
+
+        public EntityTippedArrowNPC(EntityType<? extends Arrow> types, Level level) {
+            this(types, level, null);
+        }
+
+        public EntityTippedArrowNPC(EntityType<? extends Arrow> types, Level level, NPC npc) {
+            super(types, level);
+            this.npc = (CitizensNPC) npc;
+        }
+
+        @Override
+        public CraftEntity getBukkitEntity() {
+            if (npc != null && !(super.getBukkitEntity() instanceof NPCHolder)) {
+                NMSImpl.setBukkitEntity(this, new TippedArrowNPC(this));
+            }
+            return super.getBukkitEntity();
+        }
+
+        @Override
+        public NPC getNPC() {
+            return npc;
+        }
+
+        @Override
+        public void push(double x, double y, double z) {
+            Vector vector = Util.callPushEvent(npc, x, y, z);
+            if (vector != null) {
+                super.push(vector.getX(), vector.getY(), vector.getZ());
+            }
+        }
+
+        @Override
+        public void push(Entity entity) {
+            // this method is called by both the entities involved - cancelling
+            // it will not stop the NPC from moving.
+            super.push(entity);
+            if (npc != null) {
+                Util.callCollisionEvent(npc, entity.getBukkitEntity());
+            }
+        }
+
+        @Override
+        public boolean save(CompoundTag save) {
+            return npc == null ? super.save(save) : false;
+        }
+
+        @Override
+        public Entity teleportTo(ServerLevel worldserver, BlockPos location) {
+            if (npc == null)
+                return super.teleportTo(worldserver, location);
+            return NMSImpl.teleportAcrossWorld(this, worldserver, location);
+        }
+
+        @Override
+        public void tick() {
+            if (npc != null) {
+                npc.update();
+            } else {
+                super.tick();
+            }
+        }
+    }
+
+    public static class TippedArrowNPC extends CraftTippedArrow implements ForwardingNPCHolder {
+        public TippedArrowNPC(EntityTippedArrowNPC entity) {
+            super((CraftServer) Bukkit.getServer(), entity);
+        }
+    }
+}
