052b411993d0bfcc81094a9110a1e5586ef891fa
fullwall <fullwall25@gmail.com>
2023-01-05 20:05:01 +0800

    Remove old files

--- src/main/java/net/citizensnpcs/api/SkullMetaProvider.java
+++
@@ -1,9 +0,0 @@
-package net.citizensnpcs.api;
-
-import org.bukkit.inventory.meta.SkullMeta;
-
-public interface SkullMetaProvider {
-    public String getTexture(SkullMeta meta);
-
-    public void setTexture(String string, SkullMeta meta);
-}
