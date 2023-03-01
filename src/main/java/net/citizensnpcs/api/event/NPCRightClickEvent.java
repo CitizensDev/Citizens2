b1c8fc88283e118c061a7a6c889dd5c0e1d985b2
fullwall <fullwall25@gmail.com>
2023-03-01 23:05:47 +0800

    Add right click eventual cancellation method

--- src/main/java/net/citizensnpcs/api/event/NPCRightClickEvent.java
+++ src/main/java/net/citizensnpcs/api/event/NPCRightClickEvent.java
@@ -1,14 +1,16 @@
 package net.citizensnpcs.api.event;
 
-import net.citizensnpcs.api.npc.NPC;
-
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
+
+import net.citizensnpcs.api.npc.NPC;
 
 /**
  * Called when an NPC is right-clicked by a player.
  */
 public class NPCRightClickEvent extends NPCClickEvent {
+    private boolean toCancel;
+
     public NPCRightClickEvent(NPC npc, Player rightClicker) {
         super(npc, rightClicker);
     }
@@ -18,9 +20,17 @@ public class NPCRightClickEvent extends NPCClickEvent {
         return handlers;
     }
 
-    private static final HandlerList handlers = new HandlerList();
+    public boolean isDelayedCancellation() {
+        return toCancel;
+    }
+
+    public void setDelayedCancellation(boolean toCancel) {
+        this.toCancel = true;
+    }
 
     public static HandlerList getHandlerList() {
         return handlers;
     }
+
+    private static final HandlerList handlers = new HandlerList();
 }
