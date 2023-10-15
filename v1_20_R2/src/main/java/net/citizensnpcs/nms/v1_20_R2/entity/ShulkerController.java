ea34a18ebeaf1e93c469a1c4a6468fac0cdd487c
fullwall <fullwall25@gmail.com>
2023-10-15 11:47:20 +0800

    Remove viaversion from pom

--- v1_20_R2/src/main/java/net/citizensnpcs/nms/v1_20_R2/entity/ShulkerController.java
+++ v1_20_R2/src/main/java/net/citizensnpcs/nms/v1_20_R2/entity/ShulkerController.java
@@ -15,7 +15,6 @@ import net.citizensnpcs.npc.ai.NPCHolder;
 import net.citizensnpcs.util.NMS;
 import net.citizensnpcs.util.Util;
 import net.minecraft.core.BlockPos;
-
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
