0db8f18a8223b84f7552f45b6b74d4413a7f1612
lilingfengdev <145678359+lilingfengdev@users.noreply.github.com>
2024-01-30 15:47:09 +0800

    Update BeeTrait.java

--- main/src/main/java/net/citizensnpcs/trait/versioned/BeeTrait.java
+++ main/src/main/java/net/citizensnpcs/trait/versioned/BeeTrait.java
@@ -63,7 +63,7 @@ public class BeeTrait extends Trait {
     @Command(
             aliases = { "npc" },
             usage = "bee (-s/-n) --anger anger",
-            desc = "Sets bee modifiers",
+            desc = "设置蜜蜂特征",
             modifiers = { "bee" },
             min = 1,
             max = 1,
