155d9c9730f4cfffb2b5b5aee68ca81b5e3a9ab4
fullwall <fullwall25@gmail.com>
2024-07-19 23:49:33 +0800

    Add inventory trait in horseequipper

--- main/src/main/java/net/citizensnpcs/editor/HorseEquipper.java
+++ main/src/main/java/net/citizensnpcs/editor/HorseEquipper.java
@@ -4,11 +4,13 @@ import org.bukkit.entity.Player;
 import org.bukkit.entity.Tameable;
 
 import net.citizensnpcs.api.npc.NPC;
+import net.citizensnpcs.api.trait.trait.Inventory;
 import net.citizensnpcs.util.NMS;
 
 public class HorseEquipper implements Equipper {
     @Override
     public void equip(Player equipper, NPC toEquip) {
+        toEquip.getOrAddTrait(Inventory.class);
         Tameable horse = (Tameable) toEquip.getEntity();
         NMS.openHorseScreen(horse, equipper);
     }
