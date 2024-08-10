57df21d13b4ff43e06ea02ad610c0c5e8f677931
fullwall <fullwall25@gmail.com>
2024-08-11 07:13:57 +0800

    Set wolf armor in BODY slot

--- main/src/main/java/net/citizensnpcs/editor/WolfEquipper.java
+++ main/src/main/java/net/citizensnpcs/editor/WolfEquipper.java
@@ -1,21 +1,21 @@
 package net.citizensnpcs.editor;
 
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Wolf;
 import org.bukkit.inventory.ItemStack;
 
 import net.citizensnpcs.api.npc.NPC;
+import net.citizensnpcs.api.trait.trait.Equipment;
+import net.citizensnpcs.api.trait.trait.Equipment.EquipmentSlot;
 
 public class WolfEquipper implements Equipper {
     @Override
     public void equip(Player equipper, NPC toEquip) {
         ItemStack hand = equipper.getInventory().getItemInHand();
-        Wolf wolf = (Wolf) toEquip.getEntity();
         if (hand.getType().name().equals("WOLF_ARMOR")) {
             ItemStack armor = hand.clone();
             hand.setAmount(hand.getAmount() - 1);
             armor.setAmount(1);
-            wolf.getEquipment().setChestplate(armor);
+            toEquip.getOrAddTrait(Equipment.class).set(EquipmentSlot.BODY, armor);
             equipper.getInventory().setItemInHand(hand);
         }
     }
