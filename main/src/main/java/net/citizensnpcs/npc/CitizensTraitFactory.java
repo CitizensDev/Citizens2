142785a323ed7511c98395eadd4b9e13b1e6ae4f
ZX夏夜之风 <snwcreations@qq.com>
2024-10-31 20:02:01 +0800

    fix: missed trait register code

--- main/src/main/java/net/citizensnpcs/npc/CitizensTraitFactory.java
+++ main/src/main/java/net/citizensnpcs/npc/CitizensTraitFactory.java
@@ -25,6 +25,7 @@ import net.citizensnpcs.trait.Anchors;
 import net.citizensnpcs.trait.ArmorStandTrait;
 import net.citizensnpcs.trait.AttributeTrait;
 import net.citizensnpcs.trait.BatTrait;
+import net.citizensnpcs.trait.BeTargedByTrait;
 import net.citizensnpcs.trait.BoundingBoxTrait;
 import net.citizensnpcs.trait.ClickRedirectTrait;
 import net.citizensnpcs.trait.CommandTrait;
@@ -82,6 +83,7 @@ public class CitizensTraitFactory implements TraitFactory {
         registerTrait(TraitInfo.create(AttributeTrait.class));
         registerTrait(TraitInfo.create(Anchors.class));
         registerTrait(TraitInfo.create(BatTrait.class));
+        registerTrait(TraitInfo.create(BeTargedByTrait.class));
         registerTrait(TraitInfo.create(BoundingBoxTrait.class));
         registerTrait(TraitInfo.create(ClickRedirectTrait.class));
         registerTrait(TraitInfo.create(CommandTrait.class).optInToStats());
