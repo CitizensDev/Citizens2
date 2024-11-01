3d79f327f8b8d3d6a1f3502d595efd87d1b3fba3
ZX夏夜之风 <snwcreations@qq.com>
2024-11-01 13:31:20 +0800

    feat: initial version of targetable trait

--- main/src/main/java/net/citizensnpcs/trait/TargetableTrait.java
+++ main/src/main/java/net/citizensnpcs/trait/TargetableTrait.java
@@ -1,7 +1,12 @@
 package net.citizensnpcs.trait;
 
+import net.citizensnpcs.api.exception.NPCLoadException;
+import net.citizensnpcs.api.npc.NPC;
+import net.citizensnpcs.api.npc.SimpleMetadataStore;
+import net.citizensnpcs.api.persistence.Persist;
 import net.citizensnpcs.api.trait.Trait;
 import net.citizensnpcs.api.trait.TraitName;
+import net.citizensnpcs.api.util.DataKey;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Mob;
@@ -10,17 +15,73 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
+// todo move all usages on targetable metadata to this
 @TraitName("targetable")
 public class TargetableTrait extends Trait {
     private Set<UUID> beTargetedBy;
+    @Persist
+    private Boolean state;
+    private boolean shouldSave;
 
     public TargetableTrait() {
         super("targetable");
     }
 
     @Override
+    public void onAttach() {
+        applyFromLegacyMetadata();
+    }
+
+    @Override
     public void onDespawn() {
         clearTargets();
+    }
+
+    @Override
+    public void load(DataKey key) throws NPCLoadException {
+        final Object rawState = key.getRaw("state");
+        if (rawState instanceof Boolean) {
+            state = (Boolean) rawState;
+            shouldSave = true;
+        }
+    }
+
+    @Override
+    public void save(DataKey key) {
+        if (shouldSave) {
+            if (state != null) {
+                key.setBoolean("state", state);
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        final boolean applied = applyFromLegacyMetadata();
+        if (applied) {
+            // fixme how to decide if we should persist the state? as we don't know its persistence state
+        }
+    }
+
+    public boolean isTargetable() {
+        if (state == null) {
+            return !npc.isProtected();
+        }
+        return state;
+    }
+
+    public void setTargetable(boolean state, boolean persistent) {
+        if (this.state != state) {
+            this.state = state;
+            this.shouldSave = persistent;
+            if (!state) {
+                clearTargets();
+            }
+        } else {
+            if (!shouldSave && persistent) {
+                this.shouldSave = true; // user want to persist it so here we go
+            }
+        }
     }
 
     // Only for internal use
@@ -51,4 +112,17 @@ public class TargetableTrait extends Trait {
             beTargetedBy = null;
         }
     }
+
+    private boolean applyFromLegacyMetadata() {
+        final boolean hasLegacyMeta = npc.data().has(NPC.Metadata.TARGETABLE);
+        if (hasLegacyMeta) {
+            state = npc.data().get(NPC.Metadata.TARGETABLE);
+            shouldSave = true; // because it is loaded from persistent metadata
+            // then prevent the data from being saved through metadata code, we'll handle this
+            npc.data().remove(NPC.Metadata.TARGETABLE); // fixme is this right?
+            return true;
+        }
+        return false;
+    }
+
 }
