50c9e4c411aab71a4c73e361fe4c088bf57a38a3
ZX夏夜之风 <snwcreations@qq.com>
2024-12-11 12:46:02 +0800

    fix: rewrite attribute trait load logic

--- main/src/main/java/net/citizensnpcs/trait/AttributeTrait.java
+++ main/src/main/java/net/citizensnpcs/trait/AttributeTrait.java
@@ -2,6 +2,9 @@ package net.citizensnpcs.trait;
 
 import java.util.Map;
 
+import net.citizensnpcs.api.exception.NPCLoadException;
+import net.citizensnpcs.api.util.DataKey;
+import net.citizensnpcs.util.Util;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.entity.LivingEntity;
@@ -30,18 +33,28 @@ public class AttributeTrait extends Trait {
     }
 
     @Override
+    public void load(DataKey key) throws NPCLoadException {
+        for (Map.Entry<String, Object> entry : key.getValuesDeep().entrySet()) {
+            final String rawAttributeName = entry.getKey();
+            final Attribute attribute = Util.getAttribute(rawAttributeName);
+            if (attribute != null) {
+                final Object rawValue = entry.getValue();
+                if (rawValue instanceof Double) {
+                    attributes.put(attribute, (Double) rawValue);
+                }
+            }
+        }
+    }
+
+    @Override
     public void onSpawn() {
         if (!(npc.getEntity() instanceof LivingEntity))
             return;
         LivingEntity le = (LivingEntity) npc.getEntity();
         for (Map.Entry<Attribute, Double> entry : attributes.entrySet()) {
             final Attribute key = entry.getKey();
-            if (key == null) {
-                // todo should we log an error there?
-                continue;
-            }
             final AttributeInstance attributeInstance = le.getAttribute(key);
-            if (attributeInstance == null) { // not applicable anymore so ignore // todo should we remove the entry if we reach there?
+            if (attributeInstance == null) { // not applicable anymore so ignore
                 continue;
             }
             attributeInstance.setBaseValue(entry.getValue());
