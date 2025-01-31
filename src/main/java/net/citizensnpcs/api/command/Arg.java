d76f0ac400c22c41df0397eec08f992442df6cc1
fullwall <fullwall25@gmail.com>
2025-01-31 23:26:32 +0800

    Don't print exception on OptionalKeyedCompletions class not found

--- src/main/java/net/citizensnpcs/api/command/Arg.java
+++ src/main/java/net/citizensnpcs/api/command/Arg.java
@@ -60,7 +60,6 @@ public @interface Arg {
                         }
                     }
                 } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
                 }
             }
 
