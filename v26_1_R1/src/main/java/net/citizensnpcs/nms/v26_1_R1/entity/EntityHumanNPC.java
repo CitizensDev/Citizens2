5743134c512efff55ee2aeb2f13250c79df2c038
fullwall <fullwall25@gmail.com>
2026-05-13 03:02:33 +0800

    Test commit for Folia

--- v26_1_R1/src/main/java/net/citizensnpcs/nms/v26_1_R1/entity/EntityHumanNPC.java
+++ v26_1_R1/src/main/java/net/citizensnpcs/nms/v26_1_R1/entity/EntityHumanNPC.java
@@ -289,7 +289,7 @@ public class EntityHumanNPC extends ServerPlayer implements NPCHolder, Skinnable
 
     @Override
     public boolean isInWall() {
-        if (npc == null || noPhysics || isSleeping())
+        if (npc == null || noPhysics || isSleeping() || SpigotUtil.isFoliaServer())
             return super.isInWall();
 
         return Util.inBlock(getBukkitEntity());
