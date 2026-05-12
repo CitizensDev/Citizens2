3441feac1fc75c285ff8fde57e1baebf26633c04
fullwall <fullwall25@gmail.com>
2026-05-12 23:10:10 +0800

    Fix npc knockback

--- v1_21_R7/src/main/java/net/citizensnpcs/nms/v1_21_R7/entity/EntityHumanNPC.java
+++ v1_21_R7/src/main/java/net/citizensnpcs/nms/v1_21_R7/entity/EntityHumanNPC.java
@@ -268,8 +268,7 @@ public class EntityHumanNPC extends ServerPlayer implements NPCHolder, Skinnable
         boolean damaged = super.hurtServer(level, damagesource, f);
         if (damaged && hurtMarked) {
             hurtMarked = false;
-            CitizensAPI.getScheduler().checkedRunEntityTask(EntityHumanNPC.this.getBukkitEntity(),
-                    () -> EntityHumanNPC.this.hurtMarked = true);
+           CitizensAPI.getScheduler().runEntityTask(EntityHumanNPC.this.getBukkitEntity(), () -> EntityHumanNPC.this.hurtMarked = true);
         }
         return damaged;
     }
