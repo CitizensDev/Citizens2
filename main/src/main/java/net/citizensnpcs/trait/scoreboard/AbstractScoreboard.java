f1c65c8661f7200451eec2b7361e123fb0d65008
Jonah <jonah@jsinco.dev>
2026-05-24 09:57:34 -0400

    Add Support for Scoreboard Teams on Folia (#3296)
    * Add Folia scoreboard support
    
    * Add Javadocs
    
    * Remove unnecessary changes
    
    * feat(folia): cleanup code
    
    * feat(folia): properly relocate scoreboardlibrary
    
    * feat(folia): cleanup
    
    * feat(folia): remove uncalled try-catch
    
    * feat(folia): move dummy scoreboard utility
    
    * feat(folia): use javax annotations
    
    * feat(folia): properly implement sendToPlayer
    
    * chore(26.1.2): update 26.1.1 to 26.1.2
    
    * Revert "feat(folia): properly implement sendToPlayer"
    
    This reverts commit 769f624af36d3448a169873db4f505e465850137.
    
    * chore(deps): update scoreboard api

---
+++ main/src/main/java/net/citizensnpcs/trait/scoreboard/AbstractScoreboard.java
@@ -0,0 +1,29 @@
+package net.citizensnpcs.trait.scoreboard;
+
+/**
+ * Represents a scoreboard that can be used to manage teams and their entries.
+ * This is an abstraction over the underlying {@link org.bukkit.scoreboard.Scoreboard} and
+ * {@link net.megavex.scoreboardlibrary.api.team.ScoreboardTeam} objects.
+ */
+public interface AbstractScoreboard {
+
+    /**
+     * Gets a team by name.
+     * @param name Name of the team.
+     * @return Team with the given name, or null if no such team exists.
+     */
+    AbstractTeam getTeam(String name);
+
+    /**
+     * Deletes a team from this scoreboard.
+     * @param name Name of the team.
+     */
+    void removeTeam(String name);
+
+    /**
+     * Creates a new team if it does not exist.
+     * @param name Name of the team.
+     * @return Team with the given name, or the existing team if it already exists.
+     */
+    AbstractTeam createTeam(String name);
+}
