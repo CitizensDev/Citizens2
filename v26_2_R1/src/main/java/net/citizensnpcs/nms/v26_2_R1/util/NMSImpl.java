f35f847e91427b82856466cf8a69911df4e801db
SupaYoshi <1163773+SupaYoshi@users.noreply.github.com>
2026-08-31 20:32:38 +0000

    Handle missing NMS entity when checking sneak pose

--- v26_2_R1/src/main/java/net/citizensnpcs/nms/v26_2_R1/util/NMSImpl.java
+++ v26_2_R1/src/main/java/net/citizensnpcs/nms/v26_2_R1/util/NMSImpl.java
@@ -1016,7 +1016,8 @@ public class NMSImpl implements NMSBridge {
     public boolean isSneaking(org.bukkit.entity.Entity entity) {
         if (entity instanceof Player)
             return ((Player) entity).isSneaking();
-        return getHandle(entity).getPose() == Pose.CROUCHING;
+        Entity handle = getHandle(entity);
+        return handle != null && handle.getPose() == Pose.CROUCHING;
     }
 
     @Override
